07dd4ee75489bd23e5136c7871af804d16a29e8f
Zach <zach.sterling18@gmail.com>
2021-03-29 14:18:49 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IS413_Bowling_League_ZS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
